eec521f559f1a818986e788341d8b7baecf51a14
Joel Dahne <joel@dahne.eu>
2018-12-07 16:20:13 +0100

    Initial commit
    Add parser for the T-drive dataset

---
+++ src/main/scala/parse.scala
@@ -0,0 +1,30 @@
+/* SimpleApp.scala */
+import org.apache.spark.sql.SparkSession
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object Parse {
+
+  val spark = SparkSession.builder.appName("Parser").getOrCreate()
+
+  import spark.implicits._
+
+  case class Measurement(id: Int, time: Int, x: Array[Double])
+
+  val dataFile = "/home/urathai/Datasets/public/T-drive Taxi Trajectories/release/all_data.txt"
+
+  def tdrive(dataFile: String): org.apache.spark.sql.Dataset[Measurement] = {
+
+    val format = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss")
+
+    val data = spark.read.textFile(dataFile).map{ line =>
+      val parts = line.split(",")
+      val id = parts(0).toInt
+      val t = (format.parse(parts(1)).getTime/1000).toInt
+      val x = Array(parts(2).toDouble, parts(3).toDouble)
+      Measurement(id, t, x)
+    }
+
+    return data
+  }
+}

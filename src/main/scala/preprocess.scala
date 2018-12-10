c840cee6e7245053276134ad87274ffbe086d94f
Joel Dahne <joel@dahne.eu>
2018-12-10 14:16:13 +0100

    Add class for preprocessing

---
+++ src/main/scala/preprocess.scala
@@ -0,0 +1,38 @@
+/* SimpleApp.scala */
+import org.apache.spark.sql.SparkSession
+
+object Preprocess {
+
+  val spark = SparkSession.builder.appName("Preprocesser").getOrCreate()
+
+  import spark.implicits._
+
+  val box = ((115, 117), (39, 41))
+  /**
+    * Keep only measurements whose location is inside the given box.
+   */
+  def keepBox(data: org.apache.spark.sql.Dataset[Measurement],
+    box: ((Int, Int), (Int, Int))): org.apache.spark.sql.Dataset[Measurement] = {
+    // Keep only measurements inside the given box
+    val ((lowx1, uppx1), (lowx2, uppx2)) = box
+
+    return data.filter{m => m.x(0) >= lowx1 && m.x(0) <= uppx1 &&
+      m.x(1) >= lowx2 && m.x(1) <= uppx2}
+  }
+
+  /**
+    *  Drop all measurements whose id occurs less than the given
+    *  number of type in the whole dataset.
+    */
+  def dropShort(data: org.apache.spark.sql.Dataset[Measurement],
+    limit: Int): org.apache.spark.sql.Dataset[Measurement] = {
+
+    val counts = data.groupBy("id").count
+
+    val joined = data.join(counts, "id")
+
+    val filtered = joined.filter($"count" >= limit)
+
+    return filtered.select($"id", $"time", $"x").as[Measurement]
+  }
+}

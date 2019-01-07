e6af254751018ff28e05c8cb2f05eb6731ec15d3
Joel Dahne <joel@dahne.eu>
2019-01-07 14:20:22 +0100

    Change how the Spark session is set up

--- src/main/scala/common.scala
+++ src/main/scala/common.scala
@@ -1,5 +1,9 @@
 import org.apache.spark.sql.SparkSession
 
 object SparkSessionHolder {
-  val spark = SparkSession.builder.appName("Swapmob").getOrCreate()
+  lazy val spark = SparkSession
+    .builder
+    .master("local")
+    .appName("Swapmob")
+    .getOrCreate()
 }

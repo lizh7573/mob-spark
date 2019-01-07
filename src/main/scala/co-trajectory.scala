ff9cc99d33e6c2201daf25080d65d872b37555fc
Joel Dahne <joel@dahne.eu>
2019-01-07 14:54:27 +0100

    Improve documentation for trajectories and co-trajectories

--- src/main/scala/co-trajectory.scala
+++ src/main/scala/co-trajectory.scala
@@ -5,33 +5,52 @@ import SparkSessionHolder.spark.implicits._
 
 object CoTrajectoryUtils {
 
+  /* Takes a set of measurements with IDs and returns the
+   * corresponding co-trajectory. */
   def getCoTrajectory(data: org.apache.spark.sql.Dataset[MeasurementID]):
       org.apache.spark.sql.Dataset[Trajectory] = data.groupBy("id")
     .agg(collect_set($"measurement").alias("measurements"))
     .as[Trajectory]
     .map(r => Trajectory(r.id, r.measurements.sortBy(_.time)))
 
+  /* Takes a set of grid measurements with IDs and returns the
+   * corresponding co-trajectory. */
   def getCoTrajectoryGrid(data: org.apache.spark.sql.Dataset[GridID]):
       org.apache.spark.sql.Dataset[TrajectoryGrid] = data.groupBy("id")
     .agg(collect_set($"grid").alias("grids"))
     .as[TrajectoryGrid]
     .map(r => TrajectoryGrid(r.id, r.grids.sortBy(_.time)))
 
+  /* An implicit class for a co-trajectory consisting of measurements.
+   * Most of the co-trajectory specific methods are implemented
+   * here.*/
   implicit class CoTrajectory(cotraj:
       org.apache.spark.sql.Dataset[Trajectory]) {
 
+    /* Return a Dataset with the jumpchains of the co-trajectory's
+     * trajectories. The jumpchain of a trajectory is the chain of
+     * locations for the trajectory, removing any succesive
+     * duplicates. */
     def jumpchain(partitioning: Double):
         org.apache.spark.sql.Dataset[(Int, Array[LocationPartition])] =
       cotraj.map(_.jumpchain(partitioning))
         .withColumnRenamed("_1", "id").withColumnRenamed("_2", "jumpchain")
         .as[(Int, Array[LocationPartition])]
 
+    /* Return a Dataset with the jumpchain times of the co-trajectory's
+     * trajectories. The jumpchain times of a trajectory is a list of
+     * times where each time represents the time the trajectory stays
+     * in one particular location. */
     def jumpchainTimes(partitioning: Double):
         org.apache.spark.sql.Dataset[(Int, Array[Int])] =
       cotraj.map(_.jumpchainTimes(partitioning))
         .withColumnRenamed("_1", "id").withColumnRenamed("_2", "jumpchainTimes")
         .as[(Int, Array[Int])]
 
+    /* Return a Dataset with the transitions of the co-trajectory's
+     * trajectories. The list of transition of a trajectory consists
+     * of pairs of locations together with the time spent in the first
+     * location before going to the second. */
     def transitions(partitioning: Double):
         org.apache.spark.sql.Dataset[(LocationPartition, LocationPartition,
           Long, Double)] =
@@ -42,6 +61,8 @@ object CoTrajectoryUtils {
         .withColumnRenamed("_2", "to")
         .as[(LocationPartition, LocationPartition, Long, Double)]
 
+    /* Returns an enumeration of all the partitions occurring in the
+     * co-trajectory. */
     def enumeratePartitions(partitioning: Double):
         org.apache.spark.sql.Dataset[(LocationPartition, BigInt)] =
       cotraj.flatMap(_.measurements.map(_.location.partition(partitioning)))
@@ -51,29 +72,45 @@ object CoTrajectoryUtils {
         .toDS.withColumnRenamed("_1", "location").withColumnRenamed("_2", "id")
         .as[(LocationPartition, BigInt)]
 
-    def mapMatch(): org.apache.spark.sql.Dataset[Trajectory] =
-      cotraj.mapPartitions{partition =>
+    /* Return a map matched version of the co-trajectory. Every trajectory
+     * is replaced with its map matched version. */
+    def mapMatch(): CoTrajectory = cotraj.mapPartitions{partition =>
           val mm = GraphHopperHelper.getMapMatcher
 
           partition.map(_.mapMatch(mm))
         }
   }
 
+  /* An implicit class for a co-trajectory consisting of grid
+   * measurements. Most of the co-trajectory specific methods are
+   * implemented here.*/
   implicit class CoTrajectoryGrid(cotraj:
       org.apache.spark.sql.Dataset[TrajectoryGrid]) {
 
+    /* Return a Dataset of with the jumpchains of the co-trajectorys
+     * trajectories. The jumpchain of a trajectory is the chain of
+     * locations for the trajectory, removing any succesive
+     * duplicates. */
     def jumpchain():
         org.apache.spark.sql.Dataset[(Int, Array[LocationPartition])] =
       cotraj.map(_.jumpchain)
         .withColumnRenamed("_1", "id").withColumnRenamed("_2", "jumpchain")
         .as[(Int, Array[LocationPartition])]
 
+    /* Return a Dataset with the jumpchain times of the co-trajectory's
+     * trajectories. The jumpchain times of a trajectory is a list of
+     * times where each time represents the time the trajectory stays
+     * in one particular location. */
     def jumpchainTimes():
         org.apache.spark.sql.Dataset[(Int, Array[Int])] =
       cotraj.map(_.jumpchainTimes)
         .withColumnRenamed("_1", "id").withColumnRenamed("_2", "jumpchainTimes")
         .as[(Int, Array[Int])]
 
+    /* Return a Dataset with the transitions of the co-trajectory's
+     * trajectories. The list of transition of a trajectory consists
+     * of pairs of locations together with the time spent in the first
+     * location before going to the second. */
     def transitions():
       org.apache.spark.sql.Dataset[(LocationPartition, LocationPartition,
         Long, Double)] =
--- src/main/scala/trajectory.scala
+++ src/main/scala/trajectory.scala
@@ -7,8 +7,11 @@ import scala.util.{Try, Success, Failure}
 import org.apache.spark.sql.functions._
 
 object TrajectoryHelper {
-  // Return the jumpchain list of locations. This is the chain of
-  // locations removing any duplicates.
+  /* All of the below methods assume that the given list is sorted in
+   * time */
+
+  /* Return the jumpchain for a list of locations. This is the chain of
+   * locations removing any succesive duplicates. */
   def jumpchain(locations: Array[LocationPartition]):
       Array[LocationPartition] =
     if (locations.isEmpty)
@@ -17,9 +20,9 @@ object TrajectoryHelper {
       locations(0) +: locations.sliding(2)
         .collect{ case Array(a, b) if a != b => b }.toArray
 
-  // Return the jumpchain times of a list of grid measurements. This
-  // is a list of times where each time represents the time it stays
-  // in one particular location.
+  /* Return the jumpchain times of a list of grid measurements. This is
+   * a list of times where each time represents the time it stays in
+   * one particular location. */
   def jumpchainTimes(grids: Array[Grid]):
       Array[Int] =
     if (grids.isEmpty)
@@ -33,10 +36,10 @@ object TrajectoryHelper {
             (ts, g1)
       }._1
 
-  // Returns the list of transitions for a list of grid measurements.
-  // The list of transition consists of pairs of locations together
-  // with the time spent in the first location before going to the
-  // second.
+  /* Return the list of transitions for a list of grid measurements. The
+   * list of transition consists of pairs of locations together with
+   * the time spent in the first location before going to the
+   * second. */
   def transitions(grids: Array[Grid]):
       Array[(LocationPartition, LocationPartition, Int)] =
     if (grids.length < 2)
@@ -53,28 +56,40 @@ object TrajectoryHelper {
       }._1
 }
 
-// Holds a trajectory represented by an id and an array of measurements
+/* Holds a trajectory represented by an id and an array of measurements */
 case class Trajectory(id: Int, measurements: Array[Measurement]) {
   /* The methods for Trajectory assumes that the array of Measurements
    * is sorted with respect to time. This method makes sure that this
    * is the case.*/
   def normalize(): Trajectory = Trajectory(id, measurements.sortBy(_.time))
 
+  /* Return the jumpchain of the trajectory. This is the chain of
+   * locations for the trajectory, removing any succesive
+   * duplicates. */
   def jumpchain(partitioning: Double): (Int, Array[LocationPartition]) =
     (id, TrajectoryHelper
       .jumpchain(measurements.map(_.location.partition(partitioning))))
 
+  /* Return the jumpchain times of a trajectory. This is a list of times
+   * where each time represents the time the trajectory stays in one
+   * particular location. */
   def jumpchainTimes(partitioning: Double): (Int, Array[Int]) =
     (id, TrajectoryHelper
       .jumpchainTimes(measurements
         .map(m => Grid(m.time, m.location.partition(partitioning)))))
 
+  /* Return the list of transitions for a trajectory. The list of
+   * transition consists of pairs of locations together with the time
+   * spent in the first location before going to the second. */
   def transitions(partitioning: Double):
       (Int, Array[(LocationPartition, LocationPartition, Int)]) =
     (id, TrajectoryHelper
       .transitions(measurements
         .map(m => Grid(m.time, m.location.partition(partitioning)))))
 
+  /* Return a map matched version of the trajectory. The trajectory is
+   * matched using Open Street Map data. For more details see the
+   * GraphHopperHelper class.*/
   def mapMatch(mm: com.graphhopper.matching.MapMatching =
     GraphHopperHelper.getMapMatcher): Trajectory = {
 
@@ -102,7 +117,7 @@ case class Trajectory(id: Int, measurements: Array[Measurement]) {
   }
 }
 
-// Holds a trajectory represented by an id and an array of partitions
+/* Holds a trajectory represented by an id and an array of partitions */
 case class TrajectoryGrid(id: Int, grids: Array[Grid]) {
 
   /* The methods for TrajectoryGrid assumes that the array of Grids is
@@ -110,12 +125,21 @@ case class TrajectoryGrid(id: Int, grids: Array[Grid]) {
    * the case.*/
   def normalize() = TrajectoryGrid(id, grids.sortBy(_.time))
 
+  /* Return the jumpchain of the trajectory. This is the chain of
+   * locations for the trajectory, removing any succesive
+   * duplicates. */
   def jumpchain(): (Int, Array[LocationPartition]) =
     (id, TrajectoryHelper.jumpchain(grids.map(_.location)))
 
+  /* Return the jumpchain times of a trajectory. This is a list of times
+   * where each time represents the time the trajectory stays in one
+   * particular location. */
   def jumpchainTimes(): (Int, Array[Int]) =
     (id, TrajectoryHelper.jumpchainTimes(grids))
 
+  /* Return the list of transitions for a trajectory. The list of
+   * transition consists of pairs of locations together with the time
+   * spent in the first location before going to the second. */
   def transitions():
       (Int, Array[(LocationPartition, LocationPartition, Int)]) =
     (id, TrajectoryHelper.transitions(grids))
